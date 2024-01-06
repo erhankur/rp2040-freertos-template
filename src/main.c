5e818cfa5d30a4f91a34d540e705042536d8a4e3
erhankur <erhankurubas@gmail.com>
2024-01-06 22:41:29 +0100

    example2

--- src/main.c
+++ src/main.c
@@ -1,28 +1,53 @@
-#include "FreeRTOS.h"
-#include "task.h"
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
 
+const int task_delay = 500;
+const int task_size = 128;
 
-void led_task()
-{   
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        vTaskDelay(100);
-        gpio_put(LED_PIN, 0);
-        vTaskDelay(100);
-    }
+SemaphoreHandle_t mutex;
+
+void vGuardedPrint(char *out){
+    xSemaphoreTake(mutex, portMAX_DELAY);
+    puts(out);
+    xSemaphoreGive(mutex);
 }
 
-int main()
+void vTaskSMP_print_core(void *pvParameters){
+    char *task_name = pcTaskGetName(NULL);
+    char out[12];
+
+    for (;;) {
+        sprintf(out, "%s %d", task_name, get_core_num());
+        vGuardedPrint(out);
+        vTaskDelay(task_delay);
+    }
+
+}
+
+void main()
 {
     stdio_init_all();
 
-    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
-    vTaskStartScheduler();
+    mutex = xSemaphoreCreateMutex(); // Create the mutex
 
-    while(1){};
+    // Define the task handles
+    TaskHandle_t handleA;
+    TaskHandle_t handleB;
+
+    // Create 4x tasks with different names & 2 with handles
+    xTaskCreate(vTaskSMP_print_core, "A", task_size, NULL, 1, &handleA);
+    xTaskCreate(vTaskSMP_print_core, "B", task_size, NULL, 1, &handleB);
+    xTaskCreate(vTaskSMP_print_core, "C", task_size, NULL, 1, NULL);
+    xTaskCreate(vTaskSMP_print_core, "D", task_size, NULL, 1, NULL);
+
+    // Pin Tasks
+    vTaskCoreAffinitySet(handleA, (1 << 0)); // Core 0
+    vTaskCoreAffinitySet(handleB, (1 << 1)); // Core 1
+
+    // Start the scheduler
+    vTaskStartScheduler();
 }
